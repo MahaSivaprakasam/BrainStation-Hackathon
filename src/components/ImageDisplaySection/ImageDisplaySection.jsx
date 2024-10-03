5ddca2a559a7409cfd62bf645fda015cbf08042c
MahaSivaprakasam <shreesivaprakasam@gmail.com>
2024-10-03 13:59:47 -0400

    add share button

--- src/components/ImageDisplaySection/ImageDisplaySection.jsx
+++ src/components/ImageDisplaySection/ImageDisplaySection.jsx
@@ -7,11 +7,13 @@ export default function ImageDisplaySection({query,color}){
     const API_KEY='QyWFtO1Wtd52odix0akRPtAJFVFT5l6uKpAkGjS6o5ohxaG0mXYkuU2F';
     const API_URL='https://api.pexels.com/v1/search';
     const [images,setImages]=useState([]);
+    
 
     useEffect(()=>
         {
         const getImages= async ()=>
             {
+                if (!query) return;
                 try{
                     const url=`${API_URL}?query=${query}&color=${color}&per_page=4`;
                     const response=await axios.get(url, {
@@ -34,13 +36,27 @@ export default function ImageDisplaySection({query,color}){
             </div>
         );
     }
+
+    const shareImage = (image) => {
+        if (navigator.share) {
+            navigator.share({
+                title: image.alt,
+                url: image.url,
+            })
+            .then(() => console.log('Image shared successfully'))
+            .catch((error) => console.error('Error sharing image:', error));
+        } else {
+            alert(`Share this image: ${image.url}`);
+        }
+    };
     return(
         <div>
-            {images.map(image=>{
+            {images.slice(0, 4).map(image=>{
                 return(
                     <div key={image.id} className='image-Display__item'>
-                        <img src={image.src.small} alt={image.alt} />
+                        <img src={image.src.medium} alt={image.alt} />
                         <p>Photographer: {image.photographer}</p>
+                        <button onClick={() =>shareImage(image)}>Share</button>
                         </div>
                 );
             })}
