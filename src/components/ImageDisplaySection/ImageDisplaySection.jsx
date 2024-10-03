971cf22072bf3b086c7a00231c867f6160da1ca7
MahaSivaprakasam <shreesivaprakasam@gmail.com>
2024-10-03 12:27:23 -0400

    add getImages

--- src/components/ImageDisplaySection/ImageDisplaySection.jsx
+++ src/components/ImageDisplaySection/ImageDisplaySection.jsx
@@ -3,9 +3,47 @@ import {useEffect, useState} from 'react';
 import "./ImageDisplaySection.scss";
 
     
-export default function ImageDisplaySection({query}){
+export default function ImageDisplaySection({query,color}){
     const API_KEY='QyWFtO1Wtd52odix0akRPtAJFVFT5l6uKpAkGjS6o5ohxaG0mXYkuU2F';
     const API_URL='https://api.pexels.com/v1/search';
     const [images,setImages]=useState([]);
-    
+
+    useEffect(()=>
+        {
+        const getImages= async ()=>
+            {
+                try{
+                    const url=`${API_URL}?query=${query}&color=${color}&per_page=4`;
+                    const response=await axios.get(url, {
+                        headers:{
+                            Authorization:API_KEY,
+                        },
+                    });
+                    setImages(response.data.photos);
+                }catch(error){
+                    console.error('Error fetching images:',error);
+                }
+        };
+        getImages();
+    }, [query, color]);
+
+    if(images.length===0){
+        return(
+            <div>
+                <div>No images found</div>
+            </div>
+        );
+    }
+    return(
+        <div>
+            {images.map(image=>{
+                return(
+                    <div key={image.id} className='image-Display__item'>
+                        <img src={image.src.small} alt={image.alt} />
+                        <p>Photographer: {image.photographer}</p>
+                        </div>
+                );
+            })}
+        </div>
+    )
 }
